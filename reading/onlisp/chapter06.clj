c4d1ef12d4c1dc0944426139ad26133b32db3fd5
Fogus <mefogus@gmail.com>
2013-08-06 09:16:29 -0400

    OL ch06,07

---
+++ reading/onlisp/chapter06.clj
@@ -0,0 +1,5 @@
+; Chapter 6, entitled "Functions as Representations"
+
+; As always, I will post when the code is "complete", but my progress can be followed on [Github](http://github.com/fogus/polyglot/tree/master/reading/onlisp).  Also, this post is executable, just copy and paste into a Clojure REPL.
+
+(comment I do believe that it would be more fruitful to pass over the interesting, yet esoteric, chapter 6 for the more interesting _at least for a n00b.clj like me_ chapter 7 addressing macros.)
---
+++ reading/onlisp/chapter07.clj
@@ -0,0 +1,7 @@
+; # Chapter 7, entitled "Macros"
+
+; As always, I will post when the code is "complete", but my progress can be followed on [Github](http://git\
+hub.com/fogus/polyglot/tree/master/reading/onlisp).  Also, this post is executable, just copy and paste into\
+ a Clojure REPL.
+
+; Lisps are infamous for their seemingly goofy syntax, and one can certainly sympathize with the masses in the hate for parens.  In isolation, it makes very little sense for a language to adopt the syntax of Lisp as it can be quite awkward for simple things like say, mathematical equations (although, not having to memorize a gaggle of esoteric precedence rules is a big win).  However, when one considers Lisp's macro facility, the syntax suddenly makes a whole lot of good sense.
